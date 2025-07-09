a16c8a83e42d87ebd7feb60b90346e32a32ea8b5
Sun Sunny <sol16821@gmail.com>
2025-07-09 18:53:57 +0200

    Apply background.

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,25 @@
 </script>
 
 <template>
+  <div class="background"></div>
+  <div class="filter"></div>
 </template>
 
 <style scoped>
+  .background {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    background: url(./assets/images/background.jpg);
+    filter: blur(2px);
+    z-index: -2;
+  }
 
+  .filter {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(255, 247, 195, 0.345);
+    z-index: -1;
+  }
 </style>
